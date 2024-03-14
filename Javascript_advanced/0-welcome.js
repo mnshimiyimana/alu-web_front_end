876f6056c659c8ab4212934f9ad10284eacc78e5
mnshimiyimana <m.nshimiyim@alustudent.com>
2024-03-14 21:42:54 +0200

    Advanced Javascript

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,10 @@
+//welcome function
+
+const welcome = (firstName, lastName) => {
+    const fullName = `${firstName} ${lastName}`;
+  
+    const displayFullName = () => {
+      alert(`Welcome ${fullName}!`);
+    };
+    displayFullName();
+  };
